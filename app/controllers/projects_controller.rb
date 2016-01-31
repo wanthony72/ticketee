4aff3f48c0ac948e3104c84488753e5a43176a0b
Anthony Williams <wanthony101@yahoo.co.uk>
2016-01-31 19:40:50 +0000

    tickets for project

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,4 +1,11 @@
 class ProjectsController < ApplicationController
+    
+    before_action :set_project, only: [:show,
+    :edit,
+    :update,
+    :destroy]
+        
+    
     def index
         @projects = Project.all
     end
@@ -24,15 +31,15 @@ class ProjectsController < ApplicationController
     
     
     def show
-    @project = Project.find(params[:id])
+   # @project = Project.find(params[:id])
     end
     
     def edit
-       @project = Project.find(params[:id]) 
+      # @project = Project.find(params[:id]) 
     end
     
     def update
-        @project = Project.find(params[:id])
+       # @project = Project.find(params[:id])
         if @project.update(project_params)
             flash[:notice] = "Project has been updated."
             redirect_to @project
@@ -44,7 +51,7 @@ class ProjectsController < ApplicationController
     
     
     def destroy
-        @project = Project.find(params[:id])
+        #@project = Project.find(params[:id])
         @project.destroy
         flash[:notice] = "Project has been destroyed."
         redirect_to projects_path
@@ -54,5 +61,16 @@ class ProjectsController < ApplicationController
         def project_params
         params.require(:project).permit(:name, :description)
         end
+    
+    
+    private
+        def set_project
+        @project = Project.find(params[:id])
+        rescue ActiveRecord::RecordNotFound
+        flash[:alert] = "The project you were looking" +
+        " for could not be found."
+        redirect_to projects_path
+        end
+    
     
 end
