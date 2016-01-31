a720a7667c8f549f27f77d460ebae5af9a0d9721
Anthony Williams <wanthony101@yahoo.co.uk>
2016-01-31 22:36:46 +0000

    edit tickets

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -6,6 +6,9 @@ class TicketsController < ApplicationController
     @ticket = @project.tickets.build
     end
     
+    def edit
+    end
+    
     
     def create
         @ticket = @project.tickets.build(ticket_params)
@@ -17,6 +20,19 @@ class TicketsController < ApplicationController
         render "new"
         end
     end
+    
+    def update
+        if @ticket.update(ticket_params)
+        flash[:notice] = "Ticket has been updated."
+        redirect_to [@project, @ticket]
+        else
+        flash[:alert] = "Ticket has not been updated."
+        render action: "edit"
+        end
+    end
+    
+    
+    
     private
         def ticket_params
         params.require(:ticket).permit(:title, :description)
